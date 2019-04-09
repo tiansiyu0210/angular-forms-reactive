5f45ee86e19adb365b858b8a76bc8497c0e68177
tiansiyu0210 <tiansiyu297@gmail.com>
2019-04-08 23:06:47 -0400

    status or value changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,14 @@ export class AppComponent implements OnInit{
       'gender': new FormControl('female'),
       'hobbies': new FormArray([])
     });
+
+    this.signupForm.valueChanges.subscribe(
+      (value => console.log(value))
+    );
+
+    this.signupForm.statusChanges.subscribe(
+      (status => console.log(status))
+    );
   }
 
   onSubmit() {
